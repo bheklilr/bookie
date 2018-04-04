bea208e89f1204587064af96d75479727cd38f21
Aaron Stevens <bheklilr2@gmail.com>
2018-04-03 22:31:37 -0500

    Well, this is a bit better. I can get to server:port/ without being authenticated, I think?

--- src/main/java/edu/bheklilr/bookie/config/SecurityConfig.java
+++ src/main/java/edu/bheklilr/bookie/config/SecurityConfig.java
@@ -1,16 +1,16 @@
-//package edu.bheklilr.bookie.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//
-//@Configuration
-//@EnableWebMvc
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+package edu.bheklilr.bookie.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    @Autowired
 //    private BCryptPasswordEncoder bCryptPasswordEncoder;
 //
@@ -20,5 +20,23 @@
 //    @Override
 //    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+//        auth.inMemoryAuthentication().withUser("user").password("pass").roles("USER")
+//                .and().withUser("admin").password("admin").roles("ADMIN");
 //    }
-//}
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        System.out.println("#########################################################################################");
+        System.out.println("#########################################################################################");
+        System.out.println("#########################################################################################");
+        System.out.println("#########################################################################################");
+        System.out.println("Trying to see if this even works");
+        System.out.println("#########################################################################################");
+        http
+                .authorizeRequests()
+                    .antMatchers("/", "/static/**").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll();
+    }
+}
