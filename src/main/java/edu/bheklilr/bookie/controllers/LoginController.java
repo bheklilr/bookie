73c3a3e03c959da53b58bb8af1962f93a51393a1
Aaron Stevens <bheklilr2@gmail.com>
2018-04-09 22:57:27 -0500

    Starting to figure out the form submission behavior. Referring to https://spring.io/guides/gs/handling-form-submission/

--- src/main/java/edu/bheklilr/bookie/controllers/LoginController.java
+++ src/main/java/edu/bheklilr/bookie/controllers/LoginController.java
@@ -3,6 +3,8 @@ package edu.bheklilr.bookie.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class LoginController {
@@ -10,7 +12,12 @@ public class LoginController {
 
     @RequestMapping("/login")
     public String login(Model model) {
-        model.addAttribute("activeTab", "home");
+        model.addAttribute("activeTab", "login");
         return VIEW;
+    }
+
+    @RequestMapping(value = "/do-login", method = RequestMethod.POST)
+    public RedirectView doLogin() {
+        return new RedirectView("/");
     }
 }
